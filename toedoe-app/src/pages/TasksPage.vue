079699134d03d2df433651faecec35deee51323a
tuyen <tuyennh@nhanhoagroup.vn>
2024-10-01 18:10:27 +0700

    .

---
+++ toedoe-app/src/pages/TasksPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <main style="min-height: 50vh; margin-top: 2rem">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <!-- Add new Task -->
+                    <div class="relative">
+                        <input
+                            type="text"
+                            class="form-control form-control-lg padding-right-lg"
+                            placeholder="+ Add new task. Press enter to save."
+                        />
+                    </div>
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+                    
+                    <!-- show toggle button -->
+                    <div class="text-center my-3" v-show="showToggleCompletedBtn">
+                        <button class="btn btn-sm btn-secondary" 
+                        @click="$event => showCompletedTasks = !showCompletedTasks">
+                            <span v-if="!showCompletedTasks">Show completed</span>
+                            <span v-else>Hide completed</span>
+                        </button>
+                    </div>
+
+                    <!-- list of completed tasks -->
+                    <Tasks :tasks="completedTasks" />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+import { allTasks } from '@/http/task-api';
+import Tasks from '@/components/tasks/Tasks.vue';
+
+const tasks = ref([])
+onMounted(async() => {
+    const {data} = await allTasks()
+    tasks.value = data.data
+})
+
+const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+const showToggleCompletedBtn = computed(
+    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+)
+const completedTasksIsVisible = computed(
+    () => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0
+)
+const showCompletedTasks = ref(false)
+</script>
+
---
+++ vue-composition-api/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import CartItem from './components/CartItem.vue';
+  import useCart from './useCart';
+
+  const {items, addItem, removeItem} = useCart()
+  addItem({
+    id: 1,
+    name: "Product X",
+    price: 20,
+    quantity: 1
+  })
+</script>
+
+<template>
+  <CartItem v-for="item in items" :cart-item="item" :key="item.id" @remove="removeItem" />
+</template>
+
+<style scoped>
+
+</style>
