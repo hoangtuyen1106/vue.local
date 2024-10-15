12e206ed4a0cd10c0b624d077ea51e85507695fe
tuyennh <hoangtuyen1106@gmail.com>
2024-10-15 20:19:01 +0700

    .

--- toedoe-app/src/main.js
+++ toedoe-app/src/main.js
@@ -1,9 +1,11 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import route from "./router";
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
+import './assets/main.css';
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(route);
 app.mount('#app')
