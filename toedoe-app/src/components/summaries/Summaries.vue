c0343b8cc3d0d168382c3ebeedd840720d244bed
tuyen <tuyennh@nhanhoagroup.vn>
2024-10-26 07:30:41 +0700

    .

---
+++ toedoe-app/src/components/summaries/Summaries.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p class="lead text-uppercase">{{ description }}</p>
+        <ul>
+            <li>
+                <Summary v-for="task in tasks" :key="task.id" :task="task" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import Summary from './Summary.vue';
+defineProps({
+    description: String,
+    tasks: Array
+})
+</script>
---
+++ toedoe-app/src/components/summaries/filter/FilterItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import iconCheck from '@/components/icons/iconCheck.vue';
+
+defineProps({
+    text: {
+        type: String,
+        required: true
+    },
+    selected: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <span class="de-flex justify-content-between align-items-center" :class="{'text-primary': selected}">
+        {{ text }}
+        <iconCheck v-show="selected" />
+    </span>
+</template>
